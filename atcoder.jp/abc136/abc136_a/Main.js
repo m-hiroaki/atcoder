de01e090e02b71c8ac087ab3a9aea955539ebc22
mhiroaki <m.hiroaki1217@gmail.com>
2019-08-04 21:19:55 +0900

    [AC] abc136 abc136_a

---
+++ atcoder.jp/abc136/abc136_a/Main.js
@@ -0,0 +1,16 @@
+function Main(input) {
+    input = input.split(" ").map(Number);
+
+    var A = input[0];
+    var B = input[1];
+    var C = input[2];
+
+    var ans = C - (A - B);
+
+    if (ans < 0) ans = 0;
+
+  	//出力
+	console.log(ans);
+}
+
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
