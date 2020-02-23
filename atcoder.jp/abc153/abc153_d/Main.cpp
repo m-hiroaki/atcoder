c0f0dcb84a8fcc56d0c21f487a3e9e2b54fe82c0
mhiroaki <m.hiroaki1217@gmail.com>
2020-02-23 17:43:28 +0900

    [AC] abc153 abc153_d

---
+++ atcoder.jp/abc153/abc153_d/Main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const long long MOD = 1000000007;
+long long modpow(long long a, long long n, long long m)
+{
+    long long ans = 1;
+    while (n)
+    {
+        if (n & 1)
+        {
+            ans = (ans * a) % m;
+        }
+        a = (a * a) % m;
+        n >>= 1;
+    }
+    return ans;
+}
+long long combi(long long n, long long a)
+{
+    long long ans = 1, ans1 = 1;
+    for (long long i = n - a + 1; i <= n; i++)
+    {
+        ans *= i % MOD;
+        ans %= MOD;
+    }
+
+    for (long long i = 2; i <= a; i++)
+        ans1 = (ans1 * i) % MOD;
+    ans1 = modpow(ans1, MOD - 2, MOD);
+    return ((ans % MOD) * ans1) % MOD;
+}
+
+ll func(ll H)
+{
+    if (H == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 2 * func(floor(H / 2)) + 1;
+    }
+}
+
+int main()
+{
+
+    double H;
+    ll ans = 0;
+    cin >> H;
+    ll tmp = 0;
+
+    cout << func(H) << endl;
+    return 0;
+}
