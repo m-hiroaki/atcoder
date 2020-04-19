566204931c72cca02bc559bc96d39c407c07a0a1
mhiroaki <m.hiroaki1217@gmail.com>
2020-04-19 21:28:43 +0900

    [AC] arc051 arc051_a

---
+++ atcoder.jp/arc051/arc051_a/Main.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const long long MOD = 1000000007;
+long long modpow(long long a, long long n, long long m)
+{
+    long long ans = 1;
+    while (n)
+    {
+        if (n & 1)
+        {
+            ans = (ans * a) % m;
+        }
+        a = (a * a) % m;
+        n >>= 1;
+    }
+    return ans;
+}
+long long combi(long long n, long long a)
+{
+    long long ans = 1, ans1 = 1;
+    for (long long i = n - a + 1; i <= n; i++)
+    {
+        ans *= i % MOD;
+        ans %= MOD;
+    }
+
+    for (long long i = 2; i <= a; i++)
+        ans1 = (ans1 * i) % MOD;
+    ans1 = modpow(ans1, MOD - 2, MOD);
+    return ((ans % MOD) * ans1) % MOD;
+}
+
+void dfs(string s, char mx, ll N)
+{
+    if (s.length() == (size_t)N)
+    {
+        cout << s.c_str() << endl;
+    }
+    else
+    {
+        for (char c = 'a'; c <= mx; c++)
+        {
+            dfs(s + c, ((c == mx) ? (char)(mx) : mx), N);
+        }
+    }
+}
+
+int bfs(int a, int b, int h, int w, char tmp[][101])
+{
+    int ans[101][101] = {0};
+    char c[101][101] = {0};
+    queue<pair<int, int>> s;
+
+    for (int i = 0; i <= h; i++)
+    {
+        for (int j = 0; j <= w; j++)
+        {
+            c[i][j] = tmp[i][j];
+        }
+    }
+
+    s.push(make_pair(a, b));
+
+    while (s.size() > 0)
+    {
+
+        pair<int, int> tmp = s.front();
+        s.pop();
+        c[tmp.first][tmp.second] = '#';
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (tmp.first + i < 1 || tmp.first + i > h)
+                {
+                    continue;
+                }
+                if (tmp.second + j < 1 || tmp.second + j > w)
+                {
+                    continue;
+                }
+                if (i != 0 && j != 0)
+                {
+                    continue;
+                }
+                if (i == 0 && j == 0)
+                {
+                    continue;
+                }
+                if (c[tmp.first + i][tmp.second + j] == '#')
+                {
+                    continue;
+                }
+                c[tmp.first + i][tmp.second + j] = '#';
+                if (ans[tmp.first + i][tmp.second + j] == 0)
+                {
+                    ans[tmp.first + i][tmp.second + j] = ans[tmp.first][tmp.second] + 1;
+                }
+                else
+                {
+                    ans[tmp.first + i][tmp.second + j] = min(ans[tmp.first + i][tmp.second + j], ans[tmp.first][tmp.second] + 1);
+                }
+                s.push(make_pair(tmp.first + i, tmp.second + j));
+            }
+        }
+    }
+
+    int asd = 0;
+
+    for (int i = 1; i <= h; i++)
+    {
+        for (int j = 1; j <= w; j++)
+        {
+            asd = max(a, ans[i][j]);
+        }
+    }
+
+    return asd;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (a % b == 0)
+    {
+        return (b);
+    }
+    else
+    {
+        return (gcd(b, a % b));
+    }
+}
+
+ll lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+int main()
+{
+    ll x1,y1,r,x2,y2,x3,y3;
+
+    cin >> x1 >> y1 >> r >> x2 >> y2 >> x3 >> y3;
+
+    if((x1+r <= x3) && (x1-r >= x2) && (y1-r>=y2) && (y1+r<=y3)){
+        cout << "NO" << endl
+             << "YES" << endl;
+    }
+    else if (x1 + r >= x3 && x1 - r <= x2 && y1 - r <= y2 && y1 + r >= y3)
+    {
+        if ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) <= r * r &&
+            (x3 - x1) * (x3 - x1) + (y2 - y1) * (y2 - y1) <= r * r &&
+            (x2 - x1) * (x2 - x1) + (y3 - y1) * (y3 - y1) <= r * r &&
+            (x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1) <= r * r ){
+            cout << "YES" << endl
+                 << "NO" << endl;
+            }
+            else{
+                cout << "YES" << endl
+                     << "YES" << endl;
+            }
+    }
+    else{
+        cout << "YES" << endl
+             << "YES" << endl;
+    }
+
+    return 0;
+}
